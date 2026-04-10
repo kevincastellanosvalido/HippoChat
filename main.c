ee0436d552ac1fc933f07e181363a98733794cd8
Kevin Castellanos Valido <kevincastellanosvalido@gmail.com>
2026-04-10 16:30:58 -0400

    improved design and fixed a few bugs

--- main.c
+++ main.c
@@ -6,13 +6,17 @@
 #define maxNameLength 12
 #define maxChatLength 100
 
-void clearConsole() {
+void clearConsole(){ // clears terminal
 	system("cls");
 }
 
 void clear_input_buffer() { // clears carryover input
 	int ch;
 	while ((ch = getchar()) != '\n' && ch != EOF);
+}
+
+void clear_newline_char(char string[]){ // gets rid of the newline character
+	string[strcspn(string, "\n")] = 0;
 }
 
 char* menu() {
@@ -22,7 +26,8 @@ char* menu() {
 		char* tempName = malloc(20);
 
 		printf("What's your name?\n");
-		scanf("%14s", tempName);
+		fgets(tempName, maxNameLength, stdin);
+		clear_newline_char(tempName);
 
 		if(strlen(tempName) > 12){
 			printf("Please enter a valid name\n");
@@ -41,10 +46,15 @@ char* menu() {
 int main(){
 	char* username = menu();
 	clearConsole();
-	char* chatMessage[maxChatLength + 1];
+	char chatMessage[maxChatLength + 1];
 
 	while(1){
 		printf("%s: ", username);
-		scanf("%s", chatMessage);
+		fgets(chatMessage, maxChatLength, stdin);
+		clear_newline_char(chatMessage);
+
+		if (strcmp(chatMessage, "/quit") == 0){
+			exit(1);
+		}
 	}
 }
